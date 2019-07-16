280845afc36ec4db350ef152750f4cca819d307b
Afif <afif.priya1@gmail.com>
2019-07-16 10:17:59 +0700

    v1.0

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,14 +8,19 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject circle;
     public GameObject background;
+    public GameObject exitPanel;
+    public Text exitText;
     SpriteRenderer spriteRenderer;
     Wallpaper wallpaper;
     Color currentColor;
 
+    bool exit = false;
+    float delayTime = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
+        exitPanel.SetActive(false);
         wallpaper = circle.GetComponent<Wallpaper>();
         spriteRenderer = background.GetComponent<SpriteRenderer>();
     }
@@ -25,6 +30,39 @@ public class MainMenu : MonoBehaviour
     {
         wallpaper.Spawn();
         ChangeColor();
+        Exit();
+    }
+
+    void Exit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !exit)
+        {
+            exitPanel.SetActive(true);
+            StartCoroutine(FadeOut(delayTime));
+            exit = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && exit)
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator FadeOut(float duration)
+    {
+        float counter = 0;
+        Color color = Color.white;
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            float alpha = Mathf.Lerp(1, 0, counter / duration);
+            exitText.color = new Color(1, 1, 1, alpha);
+            yield return null;
+        }
+        if (counter >= duration)
+        {
+            exitPanel.SetActive(false);
+            exit = false;
+        }
     }
 
     void ChangeColor()
